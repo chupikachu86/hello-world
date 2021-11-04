e51dc2b42209195954ac96eb837c8e277152fc11
chupikachu86 <chupikachu86@gmail.com>
2021-11-04 19:16:36 +0800

    change test.cpp 's code into calculation of reciprocal

--- test.cpp
+++ test.cpp
@@ -1,22 +1,22 @@
-// #include <iostream> // header file for std::cin
-// /*
-// The program entry
-// */
-// int main( )
-// {
-//     float a, b;
-//     std::cin >> b;
-//     if (b==0) a=b;
-//     else a=1/b;
-//     std::cout << "a is " << a;
-//     return 0;
-// }
-
-#include <iostream>
-#include <vector>
-#include <string>
-using namespace std;
-int main()
+#include <iostream> // header file for std::cin
+/*
+The program entry
+*/
+int main( )
 {
-cout<<":)"<<endl;
+    float a, b;
+    std::cin >> b;
+    if (b==0) a=b;
+    else a=1/b;
+    std::cout << "a is " << a;
+    return 0;
 }
+
+// #include <iostream>
+// #include <vector>
+// #include <string>
+// using namespace std;
+// int main()
+// {
+// cout<<":)"<<endl;
+// }
